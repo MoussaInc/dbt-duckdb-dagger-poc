1accbb44145515332a40964f63f793d4e0febdf6
moussa.mb <databirdformation@gmail.com>
2026-09-16 23:01:40 +0200

    feat: dagger command optimized: cache volume and automatic linting added

--- dagger-check.sh
+++ dagger-check.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
-dagger -c "
+
+# 1. Configuration du Cache Volume pour PIP
+# 2. Installation des dépendances et de SQLFluff
+# 3. Étape de Linting Automatique
+# 4. Étape de Build et Tests dbt (si le linting passe)
+
+dagger -c '
 container |
-from python:3.12-slim |
-with-directory /project . --exclude=.venv --exclude=target --exclude=__pycache__ --exclude=.git |
-with-workdir /project |
-with-exec -- pip install -r requirements.txt |
-with-exec -- dbt build --profiles-dir . |
-stdout
-"
+  from python:3.11-slim |
+  with-mounted-cache /root/.cache/pip pip-cache-dbt |
+  with-directory /project . --exclude=.venv --exclude=__pycache__ --exclude=.git |
+  with-workdir /project |
+  with-exec -- pip install -r requirements.txt sqlfluff dbt-core |
+  with-exec -- sqlfluff lint models/ |
+  with-exec -- dbt build --profiles-dir . |
+  stdout
+'
